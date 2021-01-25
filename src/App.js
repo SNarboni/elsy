a11b399d4d87f173b8f92003f72d5013b37cba58
SNarboni <sebastien.narboni@hotmail.fr>
2021-01-25 17:08:55 +0100

    fini

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import Steps from "./components/Steps/Steps";
+import Box from "./components/Box/Box";
+import "./styles/global.css";
 
 const tempMin = -20;
 const tempMax = 40;
@@ -10,14 +11,32 @@ const stepsMin = 0;
 const stepsMax = 50000;
 
 export class App extends React.Component {
+  data = [
+    { icon: "local_drink", color: "#3A85FF", value: 1.5, unit: "L" },
+    {
+      icon: "directions_walk",
+      color: "black",
+      value: 3000,
+      unit: "steps",
+    },
+    { icon: "favorite", color: "red", value: 120, unit: "bpm" },
+    { icon: "wb_sunny", color: "yellow", value: -10, unit: "°C" },
+  ];
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-          <Steps />
-          <p>Heart : {heartMin} </p>
-          <p>Temperature : {tempMin}</p>
-          <p>Heart : {stepsMin}</p>
+          {this.data.map(function (element) {
+            return (
+              <Box
+                icon={element.icon}
+                color={element.color}
+                value={element.value}
+                unit={element.unit}
+              />
+            );
+          })}
         </div>
       </div>
     );
